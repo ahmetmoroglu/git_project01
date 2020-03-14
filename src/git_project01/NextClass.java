bc1f2c14e1e0c393ccaf5034d43894f6c41f38e1
ahmetmoroglu <62162077+ahmetmoroglu@users.noreply.github.com>
2020-03-14 22:53:07 +0200

    commit 9

--- src/git_project01/NextClass.java
+++ src/git_project01/NextClass.java
@@ -5,7 +5,7 @@ public class NextClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello dear friends");
+		System.out.println("Hello dear SDEST");
 	}
 
 }
